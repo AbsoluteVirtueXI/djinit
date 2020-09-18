c49acc64e3ed3d9e3ae392b796ace6e477d54b07
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-09-18 10:00:23 +0200

    first commit

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk')
+const shell = require('shelljs')
 
-console.log(chalk.green('Hello!'))
+if (!shell.which('git')) {
+  shell.echo(chalk.red('Error: Sorry, this script requires git'))
+  shell.exit(1)
+}
+
+if (!shell.which('yarn')) {
+  shell.echo(chalk.red('Error: Sorry, this script requires yarn'))
+  shell.exit(1)
+}
+
+if (shell.exec('git init prout').code !== 0) {
+  shell.echo(chalk.red('Error: git init failed'))
+  shell.exit(1)
+}
+shell.echo(chalk.green('Success: Git repository initilazed'))
+
+shell.cd('prout')
+if (shell.exec('yarn init -y').code != 0) {
+  shell.echo(chalk.red('Error: yarn init failed'))
+  shell.exit(1)
+}
+shell.echo(chalk.green('Success: project initilazed'))
+shell.cp('-Rf', `${__dirname}/../templates/.*`, '.')
